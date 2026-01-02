e10af60c57a3ef599dfdb0d91a45c3e22c67253a
Wayne Graham <wayne.graham@gmail.com>
2026-01-02 12:50:38 -0500

    Add three support for 3D

---
+++ packages/scrolly-three/src/hooks/useScrollProgress.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import gsap from "gsap";
+import { ensureGSAP } from "scrolly";
+
+export function useScrollProgress(triggerEl: Element | null) {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    ensureGSAP();
+    if (!triggerEl || typeof window === "undefined") return;
+
+    const st = (gsap as any).core.globals().ScrollTrigger?.create?.({
+      trigger: triggerEl,
+      start: "top bottom",
+      end: "bottom top",
+      scrub: true,
+      onUpdate: (self: any) => setProgress(self.progress)
+    });
+
+    return () => st?.kill?.();
+  }, [triggerEl]);
+
+  return progress;
+}
---
+++ packages/scrolly-three/src/index.ts
@@ -0,0 +1,1 @@
+export { useScrollProgress } from "./hooks/useScrollProgress";
