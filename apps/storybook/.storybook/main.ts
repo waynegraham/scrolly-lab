2ed5b9d723e8084ca74c8ac738b37a8b953e6b2e
Wayne Graham <wayne.graham@gmail.com>
2026-01-02 14:40:38 -0500

    Add landing page welcome

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -2,7 +2,11 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   framework: "@storybook/react-vite",
-  stories: ["../src/**/*.stories.@(ts|tsx|js|jsx|mdx)"]
+  stories: [
+    "../src/**/*.stories.@(ts|tsx|js|jsx|mdx)",
+    "../src/**/*.mdx"
+  ],
+  addons: ["@storybook/addon-docs"]
 };
 
 export default config;
--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -3,7 +3,12 @@ import type { Preview } from "@storybook/react";
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: { matchers: { color: /(background|color)$/i, date: /Date$/ } }
+    controls: { matchers: { color: /(background|color)$/i, date: /Date$/ } },
+    options: {
+      storySort: {
+        order: ["Scrolly", ["Welcome"]]
+      }
+    }
   }
 };
 
